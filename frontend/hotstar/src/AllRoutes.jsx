538ffc8a8a2d3ad027a4f77b27ab8ef424e9041d
Sanyam Munot <sanyammunot03@gmail.com>
2025-05-02 19:02:26 +0530

    changes

--- frontend/hotstar/src/AllRoutes.jsx
+++ frontend/hotstar/src/AllRoutes.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import Footer from "./components/footer/footer";
+//import Footer from "./components/footer/footer";
 import IndividualPage from "./components/IndividualPage/IndividualPage";
 import MainPage from "./components/MainPage/MainPage";
 import Navbar from "./components/Navbar/Navbar";
 import VideoPage from "./components/videoPage/VideoPage";
 import { WatchList } from "./components/Watchlist/WatchList";
-import {Profile} from "./components/Profile/Profile"
+//import {Profile} from "./components/Profile/Profile"
 
 function AllRoutes() {
   return (
@@ -18,9 +18,9 @@ function AllRoutes() {
         <Route  path={`/:category/:id`}  element={<IndividualPage  ></IndividualPage>} ></Route>
         <Route path="/:category/:id/video" element={<VideoPage></VideoPage>}></Route>
         <Route path="/watchlist" element={<WatchList/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
+//        <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
-      <Footer></Footer>
+//      <Footer></Footer>
     </>
   );
 }
