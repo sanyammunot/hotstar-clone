009a9b248f35a97dca2d5fcb5c9d4291f9be949f
Sanyam Munot <sanyammunot03@gmail.com>
2025-05-02 18:59:56 +0530

    removed sensitive files

--- backend/src/controllers/movie.controller.js
+++ backend/src/controllers/movie.controller.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const axios = require('axios')
-
+const TMDB_API_KEY = process.env.TMDB_API_KEY
 router.get('/movies',async(req, res)=>{
     try {
         const language = req.query.language
-        const url = `https://api.themoviedb.org/3/discover/movie?api_key=3e3f0a46d6f2abc8e557d06b3fc21a77&with_original_language=${language}&language=${language}&sort_by=original_title.asc&year=2022&page=1`
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_original_language=${language}&language=${language}&sort_by=original_title.asc&year=2022&page=1`
         const response = await axios.get(url)
         return res
         .send(response.data)
@@ -20,7 +20,7 @@ router.get('/movies',async(req, res)=>{
 router.get('/tv',async(req, res)=>{
     try {
         const language = req.query.language
-        const url = `https://api.themoviedb.org/3/tv/popular?api_key=3e3f0a46d6f2abc8e557d06b3fc21a77&with_original_language=${language}&language=${language}&page=1`
+        const url = `https://api.themoviedb.org/3/tv/popular?api_key=${TMDB_API_KEY}&with_original_language=${language}&language=${language}&page=1`
         const response = await axios.get(url)
         return res
         .send(response.data)
@@ -37,7 +37,7 @@ router.get('/search',async(req, res)=>{
     try {
         const q = req.query.q
         const language = "en-US" || req.query.language
-        url = `https://api.themoviedb.org/3/search/movie?api_key=3e3f0a46d6f2abc8e557d06b3fc21a77&language=${language}&query=${q}&page=1&include_adult=false`
+        url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&language=${language}&query=${q}&page=1&include_adult=false`
         const response = await axios.get(url)
         return res
         .send(response.data)
