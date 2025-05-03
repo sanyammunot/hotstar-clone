014c76133e72553e8aaecb3497c9ea4dce1a7614
Sanyam Munot <sanyammunot03@gmail.com>
2025-05-04 00:16:31 +0530

    cors issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,8 @@ const wishlistController = require('./controllers/wishlist.controller')
 const googleController = require('./controllers/google.controller')
 const app = express()
 app.use(express.json())
-app.use(cors())
-
+// app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 
 app.use('/', movieController)
